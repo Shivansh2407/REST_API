67068ea7632ccd67f30a47323122271672a83b72
Shivansh2407 <shivanshsrivastava2000@gmail.com>
2019-09-15 09:30:06 +0530

    fixed errors and vagrant

--- src/rest_project/rest_app/views.py
+++ src/rest_project/rest_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class HelloApiView(APIView):
+    #Test API views
+
+    def get(self,request,format=None):
+    #Returns a list of Apiview features
+        apiview = [
+        'Uses HTTP methods as function (get , post , patch , put , delete)',
+        'It is similar to a traditional django view',
+        'Gives you control over your logic',
+        'Is mapped manually to URLs'
+        ]
+
+        return Response({'message':'Hello!','apiview':apiview})
