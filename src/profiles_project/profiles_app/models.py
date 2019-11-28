a123ff1553d9ca0962742ce9156e1e48e3f83981
Shivansh2407 <shivanshsrivastava2000@gmail.com>
2019-11-28 11:02:02 +0530

    Enabled the django admin for user profile mode

--- src/profiles_project/profiles_app/models.py
+++ src/profiles_project/profiles_app/models.py
@@ -16,7 +16,7 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-    def create_super_user(self , email , name , password):
+    def create_superuser(self , email , name , password):
         """Creates and saves a new superuser"""
         user = self.create_user(email , name , password)
         user.is_superuser = True
